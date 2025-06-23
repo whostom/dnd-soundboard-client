402f46dc018f6a245ae5ef571bbc7a0b1ae8a4d5
Siwek9 <72036867+Siwek9@users.noreply.github.com>
2025-06-23 12:11:06 +0200

    Make directories panel togglable

--- src/components/DirectoriesPanel.tsx
+++ src/components/DirectoriesPanel.tsx
@@ -1,15 +1,25 @@
 import { useMediaQuery } from "react-responsive";
 import DirectoryButton from "../widgets/DirectoryButton";
+import { useState } from "react";
 
 function DirectoriesPanel() {
+  const [open, setOpen] = useState<boolean>(false);
   const isMobile = useMediaQuery({
     query: "screen and (max-aspect-ratio: 1/1)",
   });
 
   return (
     <>
-      {isMobile ? <DirectoryButton /> : <></>}
-      <div id="directories-panel"></div>
+      {isMobile ? (
+        <DirectoryButton
+          onClick={(open: boolean) => {
+            setOpen(open);
+          }}
+        />
+      ) : (
+        <></>
+      )}
+      <div id="directories-panel" className={open ? "open" : undefined}></div>
     </>
   );
 }
--- src/widgets/DirectoryButton.tsx
+++ src/widgets/DirectoryButton.tsx
@@ -1,5 +1,18 @@
-function DirectoryButton() {
-  return <button id="directory-button"></button>;
+import { useState } from "react";
+
+function DirectoryButton({ onClick }: { onClick: (open: boolean) => void }) {
+  const [open, setOpen] = useState<boolean>(false);
+  return (
+    <button
+      onClick={() => {
+        console.log("siema");
+        setOpen(!open);
+        onClick(!open);
+      }}
+      className={open ? "open" : undefined}
+      id="directory-button"
+    ></button>
+  );
 }
 
 export default DirectoryButton;
